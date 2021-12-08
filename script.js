9eccce80ed1ea731bdfb389e29a81943f394f239
Nikoline Andreasen <nikolinea98@gmail.com>
2021-12-08 12:26:31 +0100

    finpudsning

--- script.js
+++ script.js
@@ -21,6 +21,42 @@
      });
    }
  });
-
 // GALLERI SLUT//
+
+//VIDEO START//
+
+function playPauseVideo() {
+  let videos = document.querySelectorAll("video");
+  videos.forEach((video) => {
+      // We can only control playback without insteraction if video is mute
+      video.muted = true;
+      // Play is a promise so we need to check we have it
+      let playPromise = video.play();
+      if (playPromise !== undefined) {
+          playPromise.then((_) => {
+              let observer = new IntersectionObserver(
+                  (entries) => {
+                      entries.forEach((entry) => {
+                          if (
+                              entry.intersectionRatio !== 1 &&
+                              !video.paused
+                          ) {
+                              video.pause();
+                          } else if (video.paused) {
+                              video.play();
+                          }
+                      });
+                  },
+                  { threshold: 1}
+              );
+              observer.observe(video);
+          });
+      }
+  });
+}
+
+// And you would kick this off where appropriate with:
+playPauseVideo();
+
+//VIDEO SLUT//
 
